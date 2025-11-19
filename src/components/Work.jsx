b880e3c0a1430c81d217d57890a0ef2ac73188e3
sanjarbekweb <sanjarbeks650@gmail.com>
2025-11-19 17:10:38 +0500

    removing project

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -11,7 +11,7 @@ const works = [
 	{
 		imgSrc: '/images/project-2.jpg',
 		title: 'Real time chat app',
-		tags: ['API', 'Socket','Backend'],
+		tags: ['API', 'Socket', 'Backend'],
 		projectLink: 'https://github.com/sanjarbekweb/real-time-chat-app',
 	},
 	{
@@ -23,40 +23,32 @@ const works = [
 	{
 		imgSrc: '/images/project-4.jpg',
 		title: 'Dictionary App',
-		tags: ['Api','Development'],
+		tags: ['Api', 'Development'],
 		projectLink: 'https://github.com/sanjarbekweb/dictionary-app',
 	},
 	{
 		imgSrc: '/images/project-5.jpg',
 		title: 'Quiz App',
-		tags: ['Fullstack','Development'],
+		tags: ['Fullstack', 'Development'],
 		projectLink: 'https://github.com/sanjarbekweb/quiz-app-full',
 	},
 	{
 		imgSrc: '/images/project-6.jpg',
-		title: 'Authorization App',
-		tags: ['Fullstack', 'Development'],
-		projectLink: 'https://github.com/sanjarbekweb/auth-fullstack',
-	},
-		{
-		imgSrc: '/images/project-6.jpg',
 		title: 'Zentry',
 		tags: ['Frontend', 'GSAP'],
 		projectLink: 'https://zentry-clone-lilac.vercel.app/',
-	}
+	},
 ]
 
 const Work = () => {
 	return (
-		<section id='work' className="section">
-			<div className="container">
-				<h2 className="headline-2 mb-8 reveal-up">
-					My portfolio highlights
-				</h2>
+		<section id='work' className='section'>
+			<div className='container'>
+				<h2 className='headline-2 mb-8 reveal-up'>My portfolio highlights</h2>
 
-				<div className="grid gap-x-4 gap-y-5 grid-cols-[repeat(auto-fill,_minmax(280px,_1fr))]">
-					{works.map(({imgSrc, title, tags, projectLink}, key) => (
-						<ProjectCard 
+				<div className='grid gap-x-4 gap-y-5 grid-cols-[repeat(auto-fill,_minmax(280px,_1fr))]'>
+					{works.map(({ imgSrc, title, tags, projectLink }, key) => (
+						<ProjectCard
 							key={key}
 							imgSrc={imgSrc}
 							title={title}
